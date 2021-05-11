113efd3a2570793a67e2f99c390152f0e675b313
Amber Forster <79550831+Ahengen@users.noreply.github.com>
2021-05-10 21:35:46 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("button#clicktoEnter").on("click", enterWord);
+    $("button#clickGenerate").on("click", generateHaiku);
+});
+
+
+//create a two-dimensional array
+
+//outer arrray index indicates number of syllables in the words in the inner array
+
+
+
+function enterWord(){
+
+}
+
+
+
+function generateHaiku(){
+
+}
